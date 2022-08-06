75cf26e046020f1a9bc21e0f0500a8994f064882
Everett Brandenburg <98061152+EverettBrand@users.noreply.github.com>
2022-08-06 13:55:49 -0700

    Update client.py

--- client.py
+++ client.py
@@ -3,15 +3,20 @@ import sys
 
 url = "http://localhost:5000/name"
 
+#returns and prints the server's information
 r = requests.post(url)
 print(r.text)
 
+#server's url
 url = "http://localhost:5000/api"
 
+#takes filepath argument from command line when running client
 path=sys.argv[1]
 
+#sends server the image's path name and returns the identified image's classification
 r = requests.post(url, json={'exp': path})
 
+#prints jsonified classification
 print('The image you’ve submitted is classified as a: ' + r.json())
 
 
